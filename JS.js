e6123474e8d092de3a1e65b139ea52af9846b144
Askar <112753402+PakhratdinovAskar@users.noreply.github.com>
2023-02-06 23:39:13 +0600

    Add files via upload

--- JS.js
+++ JS.js
@@ -1,6 +1,6 @@
 function getRandomIntInclusive(){
     min = Math.ceil(0);
-    max = Math.floor(100);
+    max = Math.floor(50);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
